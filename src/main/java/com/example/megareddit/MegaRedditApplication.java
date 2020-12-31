f01d31bb039135b1f92e6bf93fbddafaf41bb76b
John Mulongo <john.mulongo@tracom.co.ke>
2020-12-31 13:13:49 +0300

    Add comments for documentation

--- src/main/java/com/example/megareddit/MegaRedditApplication.java
+++ src/main/java/com/example/megareddit/MegaRedditApplication.java
@@ -2,8 +2,10 @@ package com.example.megareddit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class MegaRedditApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/megareddit/config/SecurityConfig.java
+++ src/main/java/com/example/megareddit/config/SecurityConfig.java
@@ -8,6 +8,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+/**
+ * EnableWebSecurity - main annotation to  enable web security in this project
+ * WebSecurityConfigurerAdapter - provides default security config we can override and customize
+ */
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -21,6 +25,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated();
     }
 
+    /**
+     * The object this method retutns should be added as a bean in the app context.
+     * Its bean ID same as method name
+     * @return
+     */
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
